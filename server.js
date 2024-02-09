80ebb13a673f88c9829a21184ce1b5b76cb69416
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-09 16:33:11 +0100

    kk

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require("./app");
 
+cron.schedule('0 * * * *', () => {
+  // Your task logic goes here
+  console.log('Cron job executed!');
+});
+
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 
