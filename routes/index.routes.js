bdc109d5124d3c169260f1512a4a331541d27b06
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-02-09 10:43:59 +0100

    new authorization

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,6 +8,39 @@ router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+
+router.get('/authorize', isAuthenticated, (req, res) => {
+  const authUrl = `https://accounts.zoho.com/oauth/v2/auth?scope=ZohoCRM.modules.READ,ZohoCRM.settings.all&client_id=1000.IR0X01MW4WTWRAKARXJ35O7J9ZUZGP&response_type=code&access_type=offline&redirect_uri=http://localhost:5005/callback`;
+  res.redirect(authUrl);
+});
+
+router.get('/callback', isAuthenticated, async (req, res) => {
+  const code = req.query.code;
+  console.log(code)
+  const tokenUrl = 'https://accounts.zoho.com/oauth/v2/token';
+  const params = new URLSearchParams({
+    code,
+    client_id: process.env.CLIENTID,
+    client_secret: process.env.CLIENTSECRET,
+    redirect_uri: process.env.REDIRECTURI,
+    grant_type: 'authorization_code',
+  });
+
+  try {
+    const response = await axios.post(tokenUrl, params);
+    const accessToken = response.data.access_token;
+    
+    // Use the access token to make API requests to Zoho CRM
+    // ...
+
+    res.send('Authenticated successfully!', response);
+  } catch (error) {
+    console.error('Error getting access token:', error.message);
+    res.status(500).send('Error getting access token');
+  }
+});
+
+
 router.get("/getData" , isAuthenticated, async (req, res) => {
   try {
     const data = await Data.find();
